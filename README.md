073de41e68c4a4217afc58c8daf4e2dd1efd218b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-10-20 09:46:52 +0200

    Add in lfp data download

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This data pipeline is used to make `.nc` files from the Steinmetz example neurophysiology data used in the Neuromatch Academy courses.
 
+## Running the Pipeline
+
+```bash
+python scripts/1_download_data.py 
+python scripts/2_convert_to_netcdf.py
+```
 
 ## Data Source:
 
--- scripts/1_download_data.py
+++ scripts/1_download_data.py
@@ -3,21 +3,20 @@ from pathlib import Path
 import requests
 from tqdm import tqdm
 
-
 urls = [
-  ("https://osf.io/agvxh/download", "steinmetz_part0.npz"),
-  ("https://osf.io/uv3mw/download", "steinmetz_part1.npz"),
-  ("https://osf.io/ehmw2/download", "steinmetz_part2.npz"),
+  ("https://osf.io/agvxh/download", "neuropixels", "steinmetz_part0.npz"),
+  ("https://osf.io/uv3mw/download", "neuropixels", "steinmetz_part1.npz"),
+  ("https://osf.io/ehmw2/download", "neuropixels", "steinmetz_part2.npz"),
+  ("https://osf.io/kx3v9/download", "lfp", "steinmetz_st.npz"),
+  ("https://osf.io/ugm9v/download", "lfp", "steinmetz_wav.npz"),
+  ("https://osf.io/kx3v9/download", "lfp", "steinmetz_lfp.npz"),
 ]
 
-base_path = Path("data/raw/neuropixels")
-base_path.mkdir(parents=True, exist_ok=True)
+base_path = Path("data/raw")
 
-for url, fname in tqdm(urls):
-    path: Path = base_path / fname
-    if path.exists():
-        continue
-  
+for url, subdir, fname in tqdm(urls):
+    path: Path = base_path / subdir / fname
+    path.parent.mkdir(parents=True, exist_ok=True)  
     r = requests.get(url)
     r.raise_for_status()
     path.write_bytes(r.content)
